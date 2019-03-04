a0e26b201c97b187d24bc18746b1539fcc75d561
Nathan Hill <nathanjameshill@gmail.com>
2019-03-04 22:32:14 +1030

    Inital commit

---
+++ Source/EscapePlan/EscapePlanGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapePlanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEPLAN_API AEscapePlanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
